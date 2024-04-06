6af8817894d8cc96344f8491213002f351b36f5f
Matej <matej.popovski@gmail.com>
2024-04-05 19:20:35 -0500

    Initial Commit

---
+++ LineLeap2/LineLeap2App.swift
@@ -0,0 +1,17 @@
+//
+//  LineLeap2App.swift
+//  LineLeap2
+//
+//  Created by Matej Popovski on 4/5/24.
+//
+
+import SwiftUI
+
+@main
+struct LineLeap2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
